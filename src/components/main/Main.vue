4e9cf6374ceffcaae732b7a59bd8b757caf6dc0d
Rosemary-z <1247562661@qq.com>
2021-01-04 14:27:35 +0800

    '登陆退出功能实现'

---
+++ src/components/main/Main.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Users from "../aside/user/Users";
+import Rights from "../aside/permission/Rights";
+import Roles from "../aside/permission/Roles";
+import Categories from "../aside/goods/Categories";
+import GoodsList from "../aside/goods/GoodsList";
+import Params from "../aside/goods/Params";
+import Orders from "../aside/order/Orders";
+import Reports from "../aside/data/Reports";
+export default {
+  components: {
+    Users,
+    Rights,
+    Roles,
+    Categories,
+    GoodsList,
+    Params,
+    Orders,
+    Reports,
+  },
+};
+</script>
+
+<style lang="less" scoped></style>
